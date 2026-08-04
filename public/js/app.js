1147ad7acde5e26181cefe9fcab36bbaf69fe22a
mostofakamal66678-cloud <mostofakamal66678@gmail.com>
2026-08-04 21:21:20 +0600

    Add swipe video feed

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,19 @@
+const videos = document.querySelectorAll("video");
+let current = 0;
 
-document.addEventListener("DOMContentLoaded", () => {
-    const video = document.querySelector("video");
+videos[current].play();
 
-    if (video) {
-        video.play().catch(() => {});
+document.addEventListener("wheel", (e) => {
+    videos[current].pause();
+
+    if (e.deltaY > 0) {
+        current = (current + 1) % videos.length;
+    } else {
+        current = (current - 1 + videos.length) % videos.length;
     }
+
+    videos[current].play();
+    videos[current].scrollIntoView({
+        behavior: "smooth"
+    });
 });
