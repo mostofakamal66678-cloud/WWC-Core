0fcb4883559c73d2e71d2a928413383b151bd274
mostofakamal66678-cloud <mostofakamal66678@gmail.com>
2026-08-28 09:40:22 +0600

    Update and rename firebase. js to firebase.js

---
+++ config/firebase.js
@@ -0,0 +1,18 @@
+// Firebase Configuration - World Wide Connect
+const firebaseConfig = {
+  apiKey: "AIzaSyCgio17aPEfR7d6juqIhn3yi6Mf65W_tO4",
+  authDomain: "world-wide-connect-62c87.firebaseapp.com",
+  projectId: "world-wide-connect-62c87",
+  storageBucket: "world-wide-connect-62c87.firebasestorage.app",
+  messagingSenderId: "93178453668",
+  appId: "1:93178453668:web:2184630caa8e61f7445031",
+  measurementId: "G-PKFJ5NEMGQ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
