204301c19d60b79205e8eba7667ce4a01cc72085
mostofakamal66678-cloud <mostofakamal66678@gmail.com>
2026-08-29 13:31:42 +0600

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,15 +1,23 @@
+// ========================================
+// FIREBASE CONFIG
+// ========================================
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCgio17aPEfR7d6juqIhn3yi6Mf65W_tO4",
-  authDomain: "world-wide-connect-62c87.firebaseapp.com",
-  projectId: "world-wide-connect-62c87",
-  storageBucket: "world-wide-connect-62c87.firebasestorage.app",
-  messagingSenderId: "93178453668",
-  appId: "1:93178453668:web:2184630caa8e61f7445031",
-  measurementId: "G-PKFJ5NEMGQ"
+    apiKey: "AIzaSyCgio17aPEfR7d6juqIhn3yi6Mf65W_tO4",
+    authDomain: "world-wide-connect-62c87.firebaseapp.com",
+    projectId: "world-wide-connect-62c87",
+    storageBucket: "world-wide-connect-62c87.firebasestorage.app",
+    messagingSenderId: "93178453668",
+    appId: "1:93178453668:web:2184630caa8e61f7445031",
+    measurementId: "G-PKFJ5NEMGQ"
 };
 
+// Firebase Initialize
 firebase.initializeApp(firebaseConfig);
 
+// Auth, Firestore, Storage
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+console.log('✅ Firebase Connected!');
